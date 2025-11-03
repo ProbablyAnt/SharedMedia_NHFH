fbb02342381788784f00789955e764fd99aea72f
ProbablyAnt <152452284+ProbablyAnt@users.noreply.github.com>
2025-11-03 20:53:59 +1100

    NHFH

---
+++ SharedMedia_NHFH/Soundpaths.lua
@@ -0,0 +1,5 @@
+local LSM = LibStub("LibSharedMedia-3.0") 
+
+LSM:Register("sound", "|cFFFF69B4NecComeToPoppa|r", [[Interface\Addons\SharedMedia_NHFH\sound\NecComeToPoppa.ogg]])
+LSM:Register("sound", "|cFFFF69B4NecOhNo|r", [[Interface\Addons\SharedMedia_NHFH\sound\NecOhNo.ogg]])
+LSM:Register("sound", "|cFFFF69B4NecStupidCunt|r", [[Interface\Addons\SharedMedia_NHFH\sound\NecStupidCunt.ogg]])
